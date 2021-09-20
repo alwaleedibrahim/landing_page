1c4082c8b86ef8021559b97377641c9392d26336
alwaleedibrahim <alwaleedibrahim@outlook.com>
2021-09-20 14:12:40 +0200

    Updated highlightSection function & removed useless functions

--- js/app.js
+++ js/app.js
@@ -1,31 +1,19 @@
 // Helper functions
 
-// Return a list of all elements with the tag "section"
-function getSectionsList () {
-	const sectionsList = document.querySelectorAll("section");
-	return sectionsList;
-}
-
-// Take a section as an argument and returns the text content of its header
-function getSectionHeader(section) {
+// Takes a section and adds it to the navigation list
+function addNavListItem (section) {
 	const header = section.querySelector("h2").innerText;
-	return header;
-}
-
-// Takes a section header and adds it to the navigation list
-function addNavListItem (sectionHeader) {
 	const navList = document.getElementById("nav_list");
 	const newItem = document.createElement("li");
-	newItem.innerText = sectionHeader;
+	newItem.innerText = header;
 	navList.appendChild(newItem);
 }
 
 // Loops through all sections in the documents and adds thier headers to the navigation list
 function constructNavList () {
-	const sections = getSectionsList();
+	const sections = document.querySelectorAll("section");
 	for (let section of sections) {
-		const header = getSectionHeader(section);
-		addNavListItem(header);
+		addNavListItem(section);
 	}
 }
 
@@ -33,26 +21,56 @@ function constructNavList () {
 // multiple elements can be visible at the same time 
 function isInViewport(element) {
     const rect = element.getBoundingClientRect();
+	// returns true if the element is fully or partially visible in the viewport
 	return (rect.bottom >= 0 && rect.top - window.innerHeight <= 0);
+}
+
+// Calculate the viewed amount of an element in percentage
+function amountOfView (element) {
+	if (isInViewport(element)) {
+		const rect = element.getBoundingClientRect();
+		// Calculate the amount using element bottom if the lower part is the visible part
+		if (rect.bottom <= rect.height) {
+			return Math.round(rect.bottom/rect.height*100);
+		}
+		// Calculate the amount using element top if the viewport is above the element top
+		else {
+			return Math.round((window.innerHeight - rect.top)/rect.height*100);
+		}
+	}
+	else {
+		return 0;
+	}
 }
 
 // Highlight section
 function highlightSection () {
+	// A variable to store the amount of view of an element
+	let view = 0;
+	// A variable to store the index of the section to be highlighted
+	let highIndex = 0;
 	const sections = document.querySelectorAll('section');
-	for (let section of sections) {
-		if (isInViewport(section)) {
-			section.classList.add("highlighted");
-		}
-		else {
-			section.classList.remove("highlighted");
+	const nav = document.querySelector("#nav_list");
+	for (let i=0; i < sections.length; i++) {
+		// Remove highlight if present from previous function call
+		sections[i].classList.remove("highlighted");
+		nav.children[i].classList.remove("active");
+		if (amountOfView(sections[i])>view) {
+			view = amountOfView(sections[i]);
+			highIndex = i;
 		}
 	}
+	sections[highIndex].classList.add("highlighted");
+	
+	// Highlight its corresponding navlist item
+	nav.children[highIndex].classList.add("active");
 }
 
+// Events
 // When DOM is ready construct the navigation list
 document.addEventListener('DOMContentLoaded', constructNavList());
 
-// Highlight 
+// Highlight sections when scrolling 
 document.addEventListener('scroll', function () {
 	highlightSection ();
 });
